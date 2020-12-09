2492b0b0487d981aeb5b60afe98e292ebc19748f
arnettha <arnettha@gmail.com>
2020-12-09 08:23:52 +0700

    change newsletter expiry time

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,7 +1,7 @@
 const EXPIRE_TIME_NEWSLETTER_HIDE = 'cermati_fe_entry_expire_time_newsletter_hide';
 
 function setNewsletterHideExpireTime() {
-  const expireTime = new Date().getTime() + 10000;
+  const expireTime = new Date().getTime() + 600000;
   localStorage.setItem(EXPIRE_TIME_NEWSLETTER_HIDE, expireTime);
 }
 
